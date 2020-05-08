eb2ab075aa2ee6e5137392c1c0d9561f6c61d57f
Andy Cheung <andychinka@gmail.com>
2020-05-08 15:52:56 +0800

    updated feature extraction details for LogMel

--- asc/preprocess.py
+++ asc/preprocess.py
@@ -32,20 +32,27 @@ class PreProcessBase(object):
 
 class LogMelPreProcess(PreProcessBase):
 
-    def __init__(self, db_path: str, feature_folder: str = "feature"):
+    def __init__(self, db_path: str, feature_folder: str,
+                 n_fft, hop_length_sec, win_length_sec, n_mels, fmax):
         super().__init__(db_path, feature_folder)
+        self.n_fft = n_fft
+        self.hop_length_sec = hop_length_sec
+        self.win_length_sec = win_length_sec
+        self.n_mels = n_mels
+        self.fmax = fmax
 
     def extract_feature(self, wave_fp: str):
-        x, sr = librosa.load(wave_fp, sr=44100, mono=True)
-        assert (x.shape == (441000,))
-
+        x, sr = librosa.load(wave_fp, sr=None, mono=True)
+        # assert (x.shape == (441000,))
+        assert (x.shape == (sr*10,))
         # 40ms winlen, half overlap
         y = librosa.feature.melspectrogram(x,
-                                           sr=44100,
-                                           n_fft=1764,
-                                           hop_length=882,
-                                           n_mels=40,
-                                           fmax=22050
+                                           sr=sr,
+                                           n_fft=self.n_fft,
+                                            hop_length=int(sr*self.hop_length_sec),
+                                            win_length=int(sr*self.win_length_sec),
+                                           n_mels=self.n_mels,
+                                           fmax=self.fmax
                                            )
         # about 1e-7
         EPS = np.finfo(np.float32).eps
@@ -56,11 +63,18 @@ class LogMelPreProcess(PreProcessBase):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-db_path', default="/media/data/shared-data/dcase/TUT-urban-acoustic-scenes-2018-mobile-development")
-    parser.add_argument("-feature_folder", default="feature")
+    parser.add_argument('-db_path', default="/media/data/shared-data/dcase/TUT-urban-acoustic-scenes-2018-development")
+    parser.add_argument("-feature_folder", default="feature2")
+
+    parser.add_argument("-n_fft", default=2048)
+    parser.add_argument("-hop_length_sec", default=0.02)
+    parser.add_argument("-win_length_sec", default=0.04)
+    parser.add_argument("-n_mels", default=40)
+    parser.add_argument("-fmax", default=24000)
 
     args = parser.parse_args()
 
-    preProcess = LogMelPreProcess(args.db_path, args.feature_folder)
+    preProcess = LogMelPreProcess(args.db_path, args.feature_folder,
+                                  args.n_fft, args.hop_length_sec, args.win_length_sec, args.n_mels, args.fmax)
     preProcess.process()
     print("done.")
